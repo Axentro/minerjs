17f32b6da9c6927b28ad0486522f0470421945fb
Kingsley <kingsleyhendrickse@me.com>
2020-12-10 16:59:03 +0000

    fixed the validate to be compatible with the crystal axentro one

--- axentro-argon2id.js
+++ axentro-argon2id.js
@@ -12,13 +12,14 @@ function bnToHex(bn) {
     var hex = bn.toString(16);
     if (hex.length % 2) { hex = '0' + hex; }
   
-    if (pos && (0x80 & parseInt(hex.slice(0, 2), 16))) {
-      hex = '00' + hex;
-    }
+    // if (pos && (0x80 & parseInt(hex.slice(0, 2), 16))) {
+    //   hex = '00' + hex;
+    // }
   
     return hex;
   }
 
+  
   function toBits(byte) { 
     var bits = [];
     for(var x=7; x>=0; x--){
@@ -35,34 +36,32 @@ function bnToHex(bn) {
     };
 
     const salt = Buffer.from(nonce);
-    
+
     const hash = await argon2.hash(block_hash, {
         type: argon2.argon2id,
         hashLength: 512,
         timeCost: 1,
-        memoryCost: 16,
+        memoryCost: 65536,
         parallelism: 1,
         raw: true,
         salt
     });
-      
-    const bits = Array.from(hash).map(toBits).flat().reverse();
+
+    const bits = Array.from(hash).map(toBits).flat();
     const leading_bits = bits.slice(0, difficulty).join("");
     return leading_bits.split("1")[0].length;
   }
 
 async function f1() {
 
-    const difficulty = 11;
-    const block_hash = 'b25872c03aa1bd57485438475b0b1a052671deee41b285742e03d76dc9b5bacd';
-
-    while(true) {
+    const difficulty = 7;
+    const block_hash = 'f60db23095ad40a0f5486e5e91e09d3475544dc3d346f4ea1d350f7708b86b0f';
+   
+    while(true){
       let nonce = Math.floor(Math.random() * 1000000000000);
-
-    //   console.log(nonce)
       const res = await validateNonce(block_hash, nonce, difficulty);
-      if (res === difficulty) {
-        console.log('matched difficulty with nonce: ' + nonce);
+      if(res === difficulty){
+        console.log('matched difficulty: ' + res + ' expected difficulty: ' + difficulty + ' with nonce: ' + nonce);
       }
     }
 }
